eb3c97ca575581a01c5a987ea480b8767de87d26
bala-amypo <bala@amypo.in>
2025-12-19 10:24:47 +0000

    User commit

--- src/main/java/com/example/demo/entity/AssetClassAllocationRule.java
+++ src/main/java/com/example/demo/entity/AssetClassAllocationRule.java
@@ -1,6 +1,8 @@
 package com.example.demo.entity;
 import com.example.demo.entity.enums.AssetClassType;
 import jakarta.persistence.*;
+
+
 @Entity 
 @Table(name = "asset_class_allocation_rules")
 public class AssetClassAllocationRule {
