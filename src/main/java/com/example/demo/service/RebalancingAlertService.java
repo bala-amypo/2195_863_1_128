1b62918eaf0b1bfd13c9c76c25f79740c84f75d8
bala-amypo <bala@amypo.in>
2025-12-21 16:51:12 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/RebalancingAlertService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.RebalancingAlertRecord;
+import java.util.List;
+
+public interface RebalancingAlertService {
+    RebalancingAlertRecord createAlert(RebalancingAlertRecord alert);
+    RebalancingAlertRecord resolveAlert(Long id);
+    List<RebalancingAlertRecord> getAlertsByInvestor(Long investorId);
+    RebalancingAlertRecord getAlertById(Long id);
+    List<RebalancingAlertRecord> getAllAlerts();
+}
