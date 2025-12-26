811b1fee1ffe7edec60951152caa7fe7b6d95143
bala-amypo <bala@amypo.in>
2025-12-26 16:09:09 +0000

    User commit

--- src/main/java/com/example/demo/controller/AllocationRuleController.java
+++ src/main/java/com/example/demo/controller/AllocationRuleController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
+@Tag(name = "Allocation Rules", description = "Asset allocation rule APIs")
+@SecurityRequirement(name = "bearerAuth")
+
+
 @RestController
 @RequestMapping("/api/rules")
 public class AllocationRuleController {
