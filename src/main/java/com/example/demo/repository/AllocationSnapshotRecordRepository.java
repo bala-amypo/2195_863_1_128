aaad7806591883253c2d9761ab068107879c379b
bala-amypo <bala@amypo.in>
2025-12-26 08:48:08 +0000

    User commit

--- src/main/java/com/example/demo/repository/AllocationSnapshotRecordRepository.java
+++ src/main/java/com/example/demo/repository/AllocationSnapshotRecordRepository.java
@@ -1,9 +1,1 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.AllocationSnapshotRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AllocationSnapshotRecordRepository extends JpaRepository<AllocationSnapshotRecord, Long> {
-}
+public interface AllocationSnapshotRecordRepository extends JpaRepository<AllocationSnapshotRecord, Long> {}
