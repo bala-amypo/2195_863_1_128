6f2f40af0f914c26e05a35e07345243017d3ba64
bala-amypo <bala@amypo.in>
2025-12-26 08:24:09 +0000

    User commit

--- src/main/java/com/example/demo/repository/AssetClassAllocationRuleRepository.java
+++ src/main/java/com/example/demo/repository/AssetClassAllocationRuleRepository.java
@@ -1,17 +1,13 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.AssetClassAllocationRule;
-import com.example.demo.entity.enums.AssetClassType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
-@Repository
 public interface AssetClassAllocationRuleRepository
         extends JpaRepository<AssetClassAllocationRule, Long> {
 
     List<AssetClassAllocationRule> findByInvestorId(Long investorId);
+
+    List<AssetClassAllocationRule> findByInvestorIdAndActiveTrue(Long investorId);
 }
