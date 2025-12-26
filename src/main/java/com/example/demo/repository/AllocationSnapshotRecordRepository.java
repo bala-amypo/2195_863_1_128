dd369aeb0dde9891b7d037064ed8223a268a17e8
bala-amypo <bala@amypo.in>
2025-12-26 08:12:09 +0000

    User commit

--- src/main/java/com/example/demo/repository/AllocationSnapshotRecordRepository.java
+++ src/main/java/com/example/demo/repository/AllocationSnapshotRecordRepository.java
@@ -4,9 +4,6 @@ import com.example.demo.entity.AllocationSnapshotRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface AllocationSnapshotRecordRepository extends JpaRepository<AllocationSnapshotRecord, Long> {
-    List<AllocationSnapshotRecord> findByInvestorId(Long investorId);
 }
--- src/main/java/com/example/demo/repository/HoldingRecordRepository.java
+++ src/main/java/com/example/demo/repository/HoldingRecordRepository.java
@@ -1,17 +1,12 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.HoldingRecord;
-import com.example.demo.entity.enums.AssetClassType;
+import com.example.demo.entity.AllocationSnapshotRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface HoldingRecordRepository extends JpaRepository<HoldingRecord, Long> {
-    List<HoldingRecord> findByInvestorId(Long investorId);
-
-
+public interface AllocationSnapshotRecordRepository extends JpaRepository<AllocationSnapshotRecord, Long> {
+    List<AllocationSnapshotRecord> findByInvestorId(Long investorId);
 }
